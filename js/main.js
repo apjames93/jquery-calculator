6879ee430cd3dbfed3456b28018e7c0f45c46770
Alexander James <AlexJames@Alexanders-MacBook-Air.local>
2016-05-04 08:40:07 -0600

    cleaned it up

--- js/main.js
+++ js/main.js
@@ -1,21 +1,17 @@
 $(document).ready(function() {
-    var array = [];
-    var divide= $(".operator")[1].innerHTML = "/";
-    var multiply =$(".operator")[2].innerHTML = "*";
+    var divide = $(".operator")[1].innerHTML = "/";
+    var multiply = $(".operator")[2].innerHTML = "*";
     $("span").click(function() {
-      var number = $("#screen").text()
+        var number = $("#screen").text()
         if ($(this).text() === "=") {
-          var total = eval(number);
-          $("#screen").empty();
+            var total = eval(number);
+            $("#screen").empty();
             return $("#screen").append(total)
-        }if($(this).text() === "÷"){
-          divide
-        }if($(this).text() === "x"){
-          multiply
-      }else{
-          $("#screen").append($(this).text()).not($("#cancel"));}
+        } else {
+            $("#screen").append($(this).text())
+        }
     })
-    $("#cancel").click(function(){
+    $("#cancel").click(function() {
         $("#screen").empty();
-      })
+    })
 })
